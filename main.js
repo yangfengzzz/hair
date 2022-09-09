33caeac5f27fe4bb09d3e6b87aefcfa4a12a096c
yangfengzzz <yangfengzzz@hotmail.com>
2022-09-09 08:37:26 +0800

    save

--- main.js
+++ main.js
@@ -3,10 +3,14 @@
 /**---------------- Hair App --------------------------------------------------*/
 // import "./src/hair-manual/HairManualApp"
 // import "./src/hair-pbr/HairPBRApp"
+
+/** original pbr hair model */
+// import "./src/hair-kajiya/no-ibl/ModelApp";
+/** kajiya+ material ball */
+// import "./src/hair-kajiya/no-ibl/MaterialBallApp";
+/** kajiya+ shading without ibl light */
 // import "./src/hair-kajiya/no-ibl/AnisoHairApp";
-// import "./src/hair-kajiya/no-ibl/HairApp";
-// import "./src/hair-kajiya/no-ibl/SphereApp";
-// import "./src/hair-kajiya/no-ibl/App";
+// import "./src/hair-kajiya/no-ibl/AnisoHairGUIApp";
 
 /**---------------- Grid App --------------------------------------------------*/
 import "./src/grid/GridApp";
